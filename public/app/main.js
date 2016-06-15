bd14d9b9e138fa1fe8c72df318ee9d59c8467586
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-06-15 10:15:14 -0500

    setup routes,first 2 html partials, linked the title h1 to the landing page, and linked the first convert button to the results page.

--- public/app/main.js
+++ public/app/main.js
@@ -1,43 +1,36 @@
 angular.module('app', ['ngRoute', 'ui.bootstrap'])
 	.config(() => (
 		firebase.initializeApp({
-			apiKey: "AIzaSyDe1hquqY6Bf1LqTkXO4tLQTWI0UpbpaLE",
-			authDomain: "heighttohuman.firebaseapp.com",
-			databaseURL: "https://heighttohuman.firebaseio.com",
-			storageBucket: "heighttohuman.appspot.com"
+			apiKey: 'AIzaSyDe1hquqY6Bf1LqTkXO4tLQTWI0UpbpaLE',
+			authDomain: 'heighttohuman.firebaseapp.com',
+			databaseURL: 'https://heighttohuman.firebaseio.com',
+			storageBucket: 'heighttohuman.appspot.com'
 			})
 	))
 
-	.controller('MainCtrl', function($http, $scope) {
+	.controller('MainCtrl', function($http, $scope, $location) {
 		const main = this;
 		const personUrl = 'https://heighttohuman.firebaseio.com/person';
 		const structureUrl = 'https://heighttohuman.firebaseio.com/structure';
-		main.heading = "HEIGHT TO HUMAN";
-		// main.Math = window.Math;
+
+		//Convert user selected data, result = the selectd structure measured in
+		//the selected person's height. Also, on click route to results page.
 		main.convertData = function (selectedPerson, selectedStructure) {
-			console.log("click");
 			main.conversion = Number($scope.selectedStructure) / Number($scope.selectedPerson);
 			console.log(main.conversion);
+			$location.path('/results');
 		}
 
 		//Get data
 		$http.get(`${personUrl}.json`)
 			.then((response) => {
-    		console.log(`${personUrl}.json`, response.data)
-      	main.people = response.data;
+				main.people = response.data;
       })
 
-    $http.get(`${structureUrl}.json`)
+		$http.get(`${structureUrl}.json`)
 			.then((response) => {
-				console.log(`${structureUrl}.json`, response.data)
 				main.structs = response.data;
-    	})
+			})
 
 
   })
-
-	// 	function() {
-	// 		firebase.database().ref('/person')
-	// 	}
-
-	// })
--- public/app/route.js
+++ public/app/route.js
@@ -1,2 +1,15 @@
 angular.module('app')
+	.config(($routeProvider) => (
+		$routeProvider
+			.when('/', {
+				controller: 'MainCtrl',
+				controllerAs: 'main',
+				templateUrl: 'app/partials/landing.html'
+			})
+			.when('/results', {
+				controller: 'MainCtrl',
+				controllerAs: 'main',
+				templateUrl: 'app/partials/results.html'
+			})
 
+	))
